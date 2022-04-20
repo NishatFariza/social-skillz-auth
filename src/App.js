c451ebe5066b4aad7a736b2b75488fed5adf1743
NishatFariza <nishatfarzia78@gmail.com>
2022-04-21 02:51:01 +0600

    navbar style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './pages/shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
+       <Header></Header>
+      <Routes>
+        <Route path='/'></Route>
+      </Routes>
     </div>
   );
 }
--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/images/logo .png";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <nav className="navbar px-20 py-6 flex justify-between items-center">
+      <div className="logo-wrap">
+        <img src={logo} alt="" />
+      </div>
+      <div className="text-white">
+        <Link className="nav-link mr-4 font-bold" to="/">Home</Link>
+        <Link className="nav-link mr-4 font-bold" to="/courses">Courses</Link>
+        <Link className="nav-link mr-4 font-bold" to="/about">About</Link>
+        <Link className="nav-link mr-4 font-bold" to="/blog">Blog</Link>
+        <Link className="nav-link mr-4 font-bold" to="/login">LogIn</Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
